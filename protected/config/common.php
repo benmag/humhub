7c47e685df2a9166401572a67f0215e79fb41800
benmag <benmag@users.noreply.github.com>
2017-01-31 17:15:29 +1000

    Facebook auth

--- protected/config/common.php
+++ protected/config/common.php
@@ -9,6 +9,16 @@ return [
                 ['http_address' => 'elasticsearch:9200'],
             ],
         ],
-        
+
+        'authClientCollection' => [
+            'clients' => [
+                'facebook' => [
+                    'class' => 'humhub\modules\user\authclient\Facebook',
+                    'clientId' => $_ENV['FACEBOOK_CLIENT_ID'],
+                    'clientSecret' => $_ENV['FACEBOOK_CLIENT_SECRET'],
+                    'scope' => 'user_events, user_posts, user_managed_groups',
+                ],
+            ],
+        ],
     ],
 ];
