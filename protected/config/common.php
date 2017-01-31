d7c8733c1ae94a27c05d89b08984bdb2e2afc6af
benmag <benmag@users.noreply.github.com>
2017-01-31 17:15:05 +1000

    ElasticSearch

--- protected/config/common.php
+++ protected/config/common.php
@@ -1,4 +1,14 @@
 <?php
-
 return [
+    // ...
+    'components' => [
+
+        'elasticsearch' => [
+            'class' => 'yii\elasticsearch\Connection',
+            'nodes' => [
+                ['http_address' => 'elasticsearch:9200'],
+            ],
+        ],
+        
+    ],
 ];
